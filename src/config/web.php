f8c44b9f2f56ca349f9cffb0865c49dca9c679ad
Semenov <semenov@skeeks.com>
2019-11-17 22:56:09 +0300

    dev

--- src/config/web.php
+++ src/config/web.php
@@ -6,6 +6,45 @@
  * @author Semenov Alexander <semenov@skeeks.com>
  */
 return [
+    'components' => [
+        'backendAdmin' => [
+            'menu' => [
+                'data' => [
+
+                    'other' => [
+                        'items' => [
+                            [
+                                "name"    => ['skeeks/money', 'Currencies'],
+                                "image"      => ['\skeeks\cms\money\assets\Asset', 'images/money_16_16.png'],
+                                'priority' => 600,
+
+                                'items' => [
+                                    [
+                                        "name" => ['skeeks/money', 'Currencies'],
+                                        "url"   => ["money/admin-currency"],
+                                        "image"   => ['\skeeks\cms\money\assets\Asset', 'images/money_16_16.png'],
+                                    ],
+
+                                    [
+                                        "name"          => ['skeeks/money', 'Settings'],
+                                        "url"            => [
+                                            "cms/admin-settings",
+                                            "component" => 'skeeks\cms\money\MoneyComponent',
+                                        ],
+                                        "image"            => ['skeeks\cms\assets\CmsAsset', 'images/icons/settings-big.png'],
+                                        "activeCallback" => function ($adminMenuItem) {
+                                            return (bool)(\Yii::$app->request->getUrl() == $adminMenuItem->getUrl());
+                                        },
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+
     'modules' => [
         'money' => [
             'class' => 'skeeks\cms\money\ModuleMoney',
